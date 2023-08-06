388a361c03b2d14b05e57f13e83c7d2b10f5307c
mrezk319 <mrezk319@gmail.com>
2023-08-06 18:03:29 +0300

    edit homeRepoImpl

--- lib/Features/home/presentation/view_model/featured_books/cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/view_model/featured_books/cubit/featured_books_cubit.dart
@@ -2,12 +2,13 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mvvm_clean_arch/Features/home/data/models/book_model/book_model.dart';
 import 'package:mvvm_clean_arch/Features/home/data/repos/home_repo.dart';
+import 'package:mvvm_clean_arch/Features/home/data/repos/home_repo_impl.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
   FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
-  final HomeRepo homeRepo;
+  final HomeRepoImpl homeRepo;
 
   Future<void> getFeaturedBooks() async {
     emit(FeaturedBooksInitial());
--- lib/Features/home/presentation/view_model/newest_books/cubit/newest_books_cubit.dart
+++ lib/Features/home/presentation/view_model/newest_books/cubit/newest_books_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mvvm_clean_arch/Features/home/data/repos/home_repo.dart';
+import 'package:mvvm_clean_arch/Features/home/data/repos/home_repo_impl.dart';
 
 import '../../../../data/models/book_model/book_model.dart';
 
@@ -8,7 +9,7 @@ part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
   NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
-  HomeRepo homeRepo;
+  HomeRepoImpl homeRepo;
   Future<void> getNewestBooks() async {
     emit(NewestBooksInitial());
     var result = await homeRepo.fetchNewestBooks();
